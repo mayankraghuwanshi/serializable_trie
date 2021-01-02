e9e5e1a9bd387854ab676466d58f18aca30fa779
mayank raghuvanshi <mayank1272@hotmail,com>
2021-01-02 18:21:19 +0530

    Readme updated

--- README.md
+++ README.md
@@ -20,4 +20,61 @@ which are not allowed to be inserted.
 void insert(final String word) throws Exception;
 ```
 
+```java
+/*
+This takes a string and search in the trie, if string found then returns true
+else false.
+*/
+boolean contains(final String word);
+```
+
+```java
+/*
+This returns a list of strings.
+It doesn't guarantee to returns strings in lexicographical order but tries best to
+maintain lexicographical order.
+*/
+List<String> getWords();
+```
+
+```java
+/*
+This takes a prefix string and returns all matching string (only prefix matched).
+It doesn't guarantee to returns strings in lexicographical order but tries best to
+maintain lexicographical order.
+*/
+List<String> getWordSuggestions(final String prefix);
+```
+
+```java
+/*
+This takes an address to build trie from a text file.
+text file should contains only one string in a line.
+*/
+int buildTrieFromFile(String filePath);
+```
+
+```java
+/*
+This generates a serializeData file with can be used to transmit over the network.
+*/
+void serializeTrie() throws IOException;
+```
+
+```java
+/*
+This method will take deserialize file path and rebuild the whole trie.
+*/
+void deserializeTrie(String filePath) throws Exception;
+```
+
+```java
+void clear();
+
+int size();
+
+```
+
+
+
 <img width="400px" src="/src/main/resources/TrieSketch.jpeg"/>
--- src/main/java/com/mayank/Trie/Trie.java
+++ src/main/java/com/mayank/Trie/Trie.java
@@ -12,24 +12,50 @@ public interface Trie {
     */
     void insert(final String word) throws Exception;
 
-
-
+    /*
+    This takes a string and delete it from the trie.
+    returns 1 if string is deleted and -1 if string not found.
+    */
     int delete(final String word);
 
     int size();
 
+    /*
+    This takes a string and search in the trie, if string found then returns true
+    else false.
+    */
     boolean contains(final String word);
 
+    /*
+    This returns a list of strings.
+    It doesn't guarantee to returns strings in lexicographical order but tries best to
+    maintain lexicographical order.
+    */
     List<String> getWords();
 
+    /*
+    This takes a prefix string and returns all matching string (only prefix matched).
+    It doesn't guarantee to returns strings in lexicographical order but tries best to
+    maintain lexicographical order.
+    */
     List<String> getWordSuggestions(final String prefix);
 
     void clear();
 
+    /*
+    This takes an address to build trie from a text file.
+    text file should contains only one string in a line.
+    */
     int buildTrieFromFile(String filePath);
 
+    /*
+    This generates a serializeData file with can be used to transmit over the network.
+    */
     void serializeTrie() throws IOException;
 
+    /*
+    This method will take deserialize file path and rebuild the whole trie.
+    */
     void deserializeTrie(String filePath) throws Exception;
 
 }
